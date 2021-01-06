96d9c5bd68640bade83fac4b5ee804debe1bc055
Data-Wan <ustimenko.work@gmail.com>
2021-01-06 18:48:30 +0300

    updated question for brain_progression

--- brain_games/games/logic_for_brain_progression.py
+++ brain_games/games/logic_for_brain_progression.py
@@ -19,21 +19,18 @@ def prog():
     Returns:
         dict
     """
-    progression.clear()
-    random_skip = random.randint(1, 9)
+    progression = []
+    progression.append(random.randint(1, 30))
+    random_skip = random.randint(0, 9)
+
     increase = random.randint(1, 10)
-    progression.insert(0, random.randint(1, 30))
-    random_skip = random.randint(1, 9)
-    increase = random.randint(1, 10)
-    index = 1
-    while index < 10:
+    for index in range(1, 10):
         progression.insert(index, progression[index - 1] + increase)
         index += 1
 
     answer = progression.pop(random_skip)
     progression.insert(random_skip, '..')
-
-    user_input = prompt.string(question.format(progression))
+    user_input = prompt.string(question.format(*progression))
 
     if user_input == str(answer):
 
@@ -50,7 +47,5 @@ def prog():
     }
 
 
-question = 'Question: {0}\nYour answer: '
+question = 'Question: {0} {1} {2} {3} {4} {5} {6} {7} {8} {9}\nYour answer: '
 game_rules = 'What number is missing in the progression?'
-
-progression = []
